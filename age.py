6be76fe536789f6bfed464c5e36cf55ade70be67
madaz1movsa <166149668+Kutbidinov@users.noreply.github.com>
2024-10-16 17:45:27 +0600

    !

---
+++ age.py
@@ -0,0 +1,24 @@
+# a = 4
+# d = 6574
+# g = '34'
+# h = 3.2
+#
+# print(type(a),
+#
+# print(type(d)),
+#
+#
+# print(type(g)),
+#
+# print(type(h)))
+
+
+
+
+v = 18368
+
+km = 1024
+
+print(v//km)
+
+
